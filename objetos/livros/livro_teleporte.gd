ed9ea1e94af0f102d166675cb3c1490a25c852bf
alexdsouzaf <alexsander.itj@hotmail.com>
2025-02-11 22:24:15 -0300

    implementado um efeito de particulas de chamas para a vela
    adicionado o shader de outline para os demais itens

--- objetos/livros/livro_teleporte.gd
+++ objetos/livros/livro_teleporte.gd
@@ -1,2 +1,12 @@
 extends ObjetoItem
 class_name LivroTeleporte
+
+@export var textura_sprite : Sprite2D
+
+
+func ligar_shader():
+	textura_sprite.material.set_shader_parameter("thickness",1)
+
+
+func desligar_shader():
+	textura_sprite.material.set_shader_parameter("thickness",0)
--- objetos/vela/vela.gd
+++ objetos/vela/vela.gd
@@ -1,2 +1,12 @@
 extends ObjetoItem
 class_name Vela
+
+@export var textura_sprite : AnimatedSprite2D
+
+
+func ligar_shader():
+	textura_sprite.material.set_shader_parameter("thickness",1)
+
+
+func desligar_shader():
+	textura_sprite.material.set_shader_parameter("thickness",0)
