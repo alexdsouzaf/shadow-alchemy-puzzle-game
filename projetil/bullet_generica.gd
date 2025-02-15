a9bb7a3b06d5b3d695d9b056d5bd4d0f73d951bf
alexdsouzaf <alexsander.itj@hotmail.com>
2025-02-15 15:06:14 -0300

    adicionado campo para informar o audio do tiro no resource, implementado na base de cada projetil

--- projetil/bullet_generica.gd
+++ projetil/bullet_generica.gd
@@ -2,8 +2,12 @@ extends Area2D
 class_name BulletGenerica
 
 @export var atributos : BulletResource
+@export var audio_tiro : AudioStreamPlayer
 
 var direction : Vector2
+
+func _ready() -> void:
+	_configura_audio()
 
 func _physics_process(delta: float) -> void:
 	global_position += atributos.speed * direction
@@ -11,3 +15,9 @@ func _physics_process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _configura_audio():
+	if atributos.som_tiro != null:
+		audio_tiro.stream = atributos.som_tiro #as AudioStream
+		audio_tiro.play()
--- projetil/bullet_resource.gd
+++ projetil/bullet_resource.gd
@@ -4,3 +4,4 @@ class_name BulletResource
 @export var name : String
 @export var speed : int
 @export var damage : int
+@export var som_tiro : AudioStream
