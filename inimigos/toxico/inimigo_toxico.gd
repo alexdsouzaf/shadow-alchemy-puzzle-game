cb60af554b03b039fd29b7cc4849b1a791febc59
alexdsouzaf <alexsander.itj@hotmail.com>
2025-04-02 20:56:33 -0300

    atualizacao do projeto para a versao 4.4.
    correcao de bug na colisao entre jogador e monstro para computar o dano corretamente

--- inimigos/toxico/inimigo_toxico.gd
+++ inimigos/toxico/inimigo_toxico.gd
@@ -19,3 +19,8 @@ func _process(delta: float) -> void:
 func _on_area_dano_area_entered(area):
 	if area is BulletGenerica:
 		queue_free()
+
+
+func _on_area_toxica_body_entered(body: Node2D) -> void:
+	# TODO emitir um signal?
+	pass # Replace with function body.
