4120700c2670586c147425b3774216af36394c87
alexdsouzaf <alexsander.itj@hotmail.com>
2025-04-19 19:33:22 -0300

    corrigida a logica dos timers dos espinhos, para ter um tempo entre aparecer e recolher que possa ser definido a vontade

--- objetos/espinhos/espinhos_no_chao.gd
+++ objetos/espinhos/espinhos_no_chao.gd
@@ -3,7 +3,11 @@ class_name EspinhosNoChao
 
 @export var colisionShape: CollisionShape2D
 @export var animation : AnimatedSprite2D
-@export var timer : Timer
+@export var timer_recolher : Timer
+@export var timer_aparecer : Timer
+
+@export var tempo_para_aparecer : int
+@export var tempo_para_recolher : int
 
 var estado : espinhoEstadoEnum = espinhoEstadoEnum.oculto
 
@@ -12,27 +16,34 @@ enum espinhoEstadoEnum {
 	visivel,
 }
 
+func _definir_timers():
+	timer_recolher.wait_time = tempo_para_recolher
+	timer_aparecer.wait_time = tempo_para_aparecer
+
 func _ready() -> void:
-	timer.start(10)
+	_definir_timers()
+	timer_aparecer.start()
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if animation.animation == "aparendo":
-		timer.start(5)
+	if animation.animation == "aparecendo":
+		timer_recolher.start()
 		
 	if animation.animation == "recolhendo":
-		timer.start(10)
+		timer_aparecer.start()
 
 
 func _on_timer_timeout() -> void:
 	if estado == espinhoEstadoEnum.oculto:
 		estado = espinhoEstadoEnum.visivel
-		animation.play("aparendo")
-		
-	if estado == espinhoEstadoEnum.visivel:
-		estado = espinhoEstadoEnum.oculto
-		animation.play("recolhendo")
+		animation.play("aparecendo")
 
 
 func _on_body_entered(body: Node2D) -> void:
 	# TODO  checar se o corpo que colidu possui a funcao basica pra receber dano
 	pass # Replace with function body.
+
+
+func _on_timer_recolher_timeout() -> void:
+	if estado == espinhoEstadoEnum.visivel:
+		estado = espinhoEstadoEnum.oculto
+		animation.play("recolhendo")
