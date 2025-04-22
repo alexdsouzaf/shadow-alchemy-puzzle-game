701c33e76aa0017ddb28b774f624d3b900647798
alexdsouzaf <alexsander.itj@hotmail.com>
2025-04-22 19:54:34 -0300

    removidos comentarios

--- level/main.gd
+++ level/main.gd
@@ -1,16 +1,7 @@
 extends Node2D
 
-#TODO achar um jeito de apenas criar instancias do jogador e nunca ter ele diretamente no editor
-#@onready var jogador = preload("res://jogador/lildoc.tscn")
-
 @export var textura_transicao : TextureRect
-
 
 func _ready() -> void:
 	levelManager.main = self
 	levelManager.textura_transicao = textura_transicao
-	#instancia_jogador()
-
-#func instancia_jogador():
-	#var instanciaJogador = jogador.instantiate()
-	#inventario.add_child(instanciaJogador)
