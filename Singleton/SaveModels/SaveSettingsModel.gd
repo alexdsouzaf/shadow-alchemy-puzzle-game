63b5b264ed9c5c896a9c86d60eb124b34b0c39de
alexdsouzaf <alexsander.itj@hotmail.com>
2025-04-27 10:49:49 -0300

    implementacao da tela de config de audio, criados canais de audio e model para salvar posteriormente
    criado componente de audio slider

--- Singleton/SaveModels/SaveSettingsModel.gd
+++ Singleton/SaveModels/SaveSettingsModel.gd
@@ -1,4 +1,7 @@
 extends Resource
 class_name SaveSettingsModel
 
-@export var volume_master : int
+@export var volume_master : float
+@export var volume_music : float
+@export var volume_sfx : float
+@export var volume_ui : float
