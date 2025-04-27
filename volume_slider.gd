e7e5c2d95c2c0ee0b73d3b34e4ada4789cf63af5
alexdsouzaf <alexsander.itj@hotmail.com>
2025-04-27 11:12:56 -0300

    implementado o registro das settings no arquivo de save

--- volume_slider.gd
+++ volume_slider.gd
@@ -1,14 +1,33 @@
 extends HSlider
 
-@export var bus_name : String
+enum audioBusEnum {
+	Master,
+	Music,
+	sfx,
+	UI
+}
+
+@export var audioBus : audioBusEnum
 
 var bus_index : int
 
 func _ready() -> void:
-	bus_index = AudioServer.get_bus_index(bus_name)
-	
+	bus_index = AudioServer.get_bus_index(str(audioBusEnum))
 	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
-
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+
+func _on_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		var modelo = GameSave.load_save_settings()
+		match audioBus:
+			audioBusEnum.Master:
+				modelo.volume_master = value
+			audioBusEnum.Music:
+				modelo.volume_music = value
+			audioBusEnum.sfx:
+				modelo.volume_sfx = value
+			audioBusEnum.UI:
+				modelo.volume_ui = value
+		gameSave.write_save_settings(modelo)
