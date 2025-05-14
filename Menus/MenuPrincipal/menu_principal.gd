186f3bc8327e5c293f5c8a55a16c40168f90ed59
alexdsouzaf <alexsander.itj@hotmail.com>
2025-05-13 21:48:03 -0300

    implementado funcionalidade de new game e continue ao inves de um unico botao de start

--- Menus/MenuPrincipal/menu_principal.gd
+++ Menus/MenuPrincipal/menu_principal.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+@onready var button_continue: Button = $CanvasLayer/VBoxContainer/ButtonContinue
+
+
+func _ready() -> void:
+	#carregar o save game pra saber se devo mostrar o continue
+	var saveSalas = GameSave.load_save_salas()
+	button_continue.visible = saveSalas != null && saveSalas.numero_sala > 0
 
 func _on_button_iniciar_pressed() -> void:
+	# o new game vai resetar o save das salas antes de comecar
+	gameSave.write_save_salas(SaveSalaModel.new())
+	gameSave.write_save_personagem(SavePersonagemModel.new())
 	get_tree().change_scene_to_file("res://level/main.tscn")
 
 
@@ -10,3 +20,8 @@ func _on_button_configs_pressed() -> void:
 
 func _on_button_sair_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_continue_pressed() -> void:
+	#vai iniciar normalmente
+	get_tree().change_scene_to_file("res://level/main.tscn")
